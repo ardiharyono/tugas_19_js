5eca7df5422b5edfca05ee2c5e2c9c4cb98a8467
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:34:16 +0700

    Tugas 19 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas19.js
@@ -0,0 +1,22 @@
+function keSatu() {
+  var satu = "Semangat...";
+  console.log(satu.repeat(5));
+}
+
+keSatu()
+console.log("=========================");
+
+function keDua() {
+  var dua = "*****";
+  console.log(dua);
+  console.log(dua.repeat(5));
+  console.log(dua.repeat(9));
+  console.log(dua.repeat(13));
+  console.log(dua.repeat(21));
+  console.log(dua.repeat(13));
+  console.log(dua.repeat(9));
+  console.log(dua.repeat(5));
+  console.log(dua);
+}
+
+keDua()
